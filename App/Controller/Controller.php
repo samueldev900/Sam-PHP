87c395560ccfa2ee09494a165509d9a7877f0cbf
Samuel Oliveira <samueloliveira900@gmail.com>
2025-09-30 11:53:29 -0300

    feat: adicionado data na função view

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -1,12 +1,21 @@
-<?php 
+<?php
 
 namespace App\Controller;
+
 abstract class Controller
 {
-    public static function view(string $namePage)
+    public static function view(string $namePage, array $data = [])
     {
-        $page = file_get_contents(__DIR__ . '/../../resources/views/' . $namePage . '.html');
-        echo $page;
-    }
+        $file = __DIR__ . '/../../resources/views/' . $namePage . '.html';
+        
+        if (file_exists($file)) {
+            $page = file_get_contents($file);
 
+            foreach ($data as $key => $value) {
+                $page = str_replace("{{ $key }}", $value, $page);
+            }
+
+            echo $page;
+        }
+    }
 }
--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 class UserController extends Controller
 {
     public function index(){
-        return $this->view('home');
+        
+        return $this->view('home', [
+            'nome' => 'Renata Oliveira'
+        ]);
+        
     }
 }
